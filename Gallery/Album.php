2dd1ada263fdd74177bdd72907b624bc29c61968
liezelbael <jessryll.cadorna@evsu.edu.ph>
2021-11-09 14:07:08 +0800

    second commit

---
+++ Gallery/Album.php
@@ -0,0 +1,32 @@
+<link rel="stylesheet" type="text/css" href="style.css">
+<div id="dvPic">
+	<img src="pics/a.jpg"> <br><br>
+	
+</div>
+<div id="dvPic">
+	<img src="pics/b.jpg"> <br><br>
+	
+</div>
+<div id="dvPic">
+	<img src="pics/c.jpg">  <br><br>
+	
+</div>
+<div id="dvPic">
+	<img src="pics/d.jpg"> <br><br>
+	
+</div>
+<div id="dvPic">
+	<img src="pics/e.jpg"> <br><br>
+	
+</div>
+<div id="dvPic">
+	<img src="pics/f.jpg"> <br><br>
+	
+</div>
+<div id="dvPic">
+	<img src="pics/g.jpg"> <br><br>
+	
+</div>
+<div id="dvPic">
+	<img src="pics/h.jpg"> <br><br>
+</div>
